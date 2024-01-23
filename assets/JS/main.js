5eb8507465cdd6639cd3ff6fa6fab2572760fe8b
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-23 15:51:43 +0100

    created the cycle for and created the others posts

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -73,5 +73,44 @@ const posts = [
     }
 ];
 
+const constainerHtml = document.getElementById("container")
 
-console.log("ciao mondo")
+for (let i = 0; i < posts.length; i++) {
+    let arrayPosts = posts[i]
+
+    console.log(posts[i])
+    console.log(posts[i].author.name)
+
+    constainerHtml.innerHTML += `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[i].author.image}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[i].author.name}</div>
+                    <div class="post-meta__time">${arrayPosts.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${arrayPosts.content}</div>
+        <div class="post__image">
+            <img src="${arrayPosts.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${arrayPosts.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+ `
+}
