5e288f6d389030703cc87b864ca4b412703b3ded
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-27 17:49:36 +0100

    working on the addeventListenr on the forEach

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -103,25 +103,28 @@ for (let i = 0; i < posts.length; i++) {
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="1" id="buttonLikes">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                         <span class="like-button__label">Mi Piace</span>
                     </a>
                 </div>
                 <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${arrayPosts.likes}</b> persone
+                    Piace a <b id="like-counter-${arrayPosts.id}" class="js-likes-counter">${arrayPosts.likes}</b> persone
                 </div>
             </div> 
         </div>            
     </div>
 `
-
-let button = document.getElementById("buttonLikes");
-
-button.addEventListener("click", function(){
-    console.log(posts[0].likes = 80 + 1)
-    button.classList.add("like-button--liked")
-});
-
 };
+
+const button = document.querySelectorAll(".js-like-button");
+const counterHtml = document.querySelectorAll(".js-likes-counter");
+
+button.forEach( (element, index) => {
+    element.addEventListener("click", function(e){
+        e.preventDefault()
+
+        element.classList.toggle("like-button--liked")
+    });
+})
 
